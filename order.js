07e71558a3de7c52d9a1aee0a1f1ad943c251560
Tushar Bhattacharya <codertushar@gmail.com>
2020-05-03 00:58:54 +0530

    Commit

--- order.js
+++ order.js
@@ -1,24 +1,20 @@
-function Order(quantity,price, side){
+function Order(quantity,price, side,timeStamp){
     this.quantity = quantity;
     this.side = side
     this.price =  price
+
+    this.getSide = () => { return this.type }
+    this.getPrice = () => { return this.price }
+    this.getQuantity = () => { return this.quantity }
+    this.setSide = (side) => { this.side = side }
+    this.setPrice = (price) => { this.price =  price }
+    this.setQuantity = (quantity) => { this.quantity = quantity }
+
+
 }
 
-Object.prototype.reduceQuantity = (quantity)=>{
 
-}
 
-Object.prototype.getSide = () => { return this.type }
-
-Object.prototype.getPrice = () => { return this.price }
-
-Object.prototype.getQuantity = () => { return this.quantity }
-
-Object.prototype.setSide = (side) => { this.side = side }
-
-Object.prototype.setPrice = (price) => { this.price =  price }
-
-Object.prototype.setQuantity = (quantity) => { this.quantity = quantity }
 
 
 
--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ const GLOBAL_TICKER_DATA = [
     {id:3,label:"Oracle", ticker:"ORCL"},
     {id:4,label:"Zeguro", ticker:"ZGRO"},
 ]
-let order = new Order(300,100,'b')
+let order = new Order(300,100,'b',(new Date()).getTime())
 var BUY_BOOK = {
     1:[order],
     2:[],
@@ -25,6 +25,10 @@ var SELL_BOOK = {
     3:[],
     4:[]
 }
+
+var NEW_ORDER_QUEUE = []
+
+var ACTIVITY_LOG = []
 
 
 
@@ -41,15 +45,20 @@ routes.post('/order', (req,res)=>{
             let order = req.body.order;
             // DETAIL Validation required
             if(order && order.qty && order.price && order.side ){
-                let newOrder = new Order(order.qty, order.price, order.side)
-                if(order.side == 'b'){
-                    BUY_BOOK[ticker].push(newOrder)
-                    
+                let newOrder = new Order(order.qty, order.price, order.side,(new Date()).getTime()); 
+                let status = executeOrder(newOrder,ticker)
+                if(!status){
+                    if(order.side == 'b'){
+                        BUY_BOOK[ticker].push(newOrder)
+                        
+                    }
+                    else {
+                        SELL_BOOK[ticker].push(newOrder)
+                        
+                    }
+
                 }
-                else {
-                    SELL_BOOK[ticker].push(newOrder)
-                    
-                }
+             
                 res.json({
                     'buy_book':BUY_BOOK,
                     'sell_book':SELL_BOOK
@@ -75,7 +84,96 @@ routes.post('/order', (req,res)=>{
 })
 
 routes.get('/order', (req,res)=>{
-        res.json(BOOK)
+    res.json({
+        'buy_book':BUY_BOOK,
+        'sell_book':SELL_BOOK
+    })
 })
+
+
+function executeOrder(newOrder,ticker){
+    let executeStatus = false;
+    if(newOrder.side == 'b'){
+
+        // check against sell
+        let minRestingSellOrder =  new Order(999999,999999, 'buffer');
+            minIndex = -1
+        
+            SELL_BOOK[ticker].forEach((restingOrder, i)=>{
+                if((newOrder.price <= restingOrder.price) && (restingOrder.quantity > 0)){
+                    if(restingOrder.price < minRestingSellOrder.price){
+                        minRestingSellOrder = restingOrder
+                        minIndex = i
+                    }
+                    
+                }
+            })
+            console.log(SELL_BOOK[ticker], minRestingSellOrder, newOrder)
+
+            if(minRestingSellOrder.side !== 'buffer'){
+                executeStatus = true
+                if(newOrder.quantity == minRestingSellOrder.quantity){
+
+                    SELL_BOOK[ticker].splice(minIndex,1)
+                }
+                else if( newOrder.quantity > minRestingSellOrder.quantity){
+                    let difference = newOrder.quantity - minRestingSellOrder.quantity;
+                    SELL_BOOK[ticker].splice(minIndex,1) 
+                    newOrder.setQuantity(difference)
+                    BUY_BOOK[ticker].push(newOrder)
+                }
+                else if(newOrder.quantity < minRestingSellOrder.quantity){
+                    let difference = minRestingSellOrder.quantity - newOrder.quantity
+                    minRestingSellOrder.setQuantity(difference)
+                    SELL_BOOK[ticker][minIndex] = minRestingSellOrder
+
+                }
+            }
+
+            
+
+    }
+    else if(newOrder.side == 's'){
+
+        // check against buy
+      
+      let maxRestingBuyOrder =  new Order(0,0, 'buffer');
+      maxIndex = -1
+  
+      BUY_BOOK[ticker].forEach((restingOrder, i)=>{
+          if((newOrder.price >= restingOrder.price) && (restingOrder.quantity > 0)){
+              if(restingOrder.price > maxRestingBuyOrder.price){
+                  maxRestingBuyOrder = restingOrder
+                  maxIndex = i 
+              }
+              
+          }
+      })
+      console.log(BUY_BOOK[ticker], maxRestingBuyOrder, newOrder)
+
+      if(maxRestingBuyOrder.side !== 'buffer'){
+        executeStatus = true
+          if(newOrder.quantity == maxRestingBuyOrder.quantity){
+
+              BUY_BOOK[ticker].splice(maxIndex,1)
+          }
+          else if( newOrder.quantity > maxRestingBuyOrder.quantity){
+              let difference = newOrder.quantity - maxRestingBuyOrder.quantity;
+              BUY_BOOK[ticker].splice(maxIndex,1) 
+              newOrder.setQuantity(difference)
+              SELL_BOOK[ticker].push(newOrder)
+          }
+          else if(newOrder.quantity < maxRestingBuyOrder.quantity){
+              let difference = maxRestingBuyOrder.quantity - newOrder.quantity
+              maxRestingBuyOrder.setQuantity(difference)
+              BUY_BOOK[ticker][maxIndex] = maxRestingBuyOrder
+
+          }
+      }
+    }
+
+    return executeStatus;
+
+}
 
 module.exports = routes
